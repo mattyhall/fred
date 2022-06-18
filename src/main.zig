fe887d31c99d008b9e35d8df894aded3b6dce711
Matt Hall <matthew@quickbeam.me.uk>
2022-06-18 13:17:41 +0100

    Stop unrecognised key presses in normal mode from causing a movement

--- src/main.zig
+++ src/main.zig
@@ -270,6 +270,26 @@ pub const State = struct {
                                 .end = @intCast(u32, match.end),
                             });
                         }
+                        if (matches.matches.len > 0) {
+                            const pos = matches.matches[0].start;
+                            var iter = self.buffer.lineIterator();
+                            var buf_pos: u32 = 0;
+                            var line: u32 = 0;
+                            while (iter.next()) |l| {
+                                if (buf_pos <= pos and buf_pos + @intCast(u32, l.len) + 1 > pos) {
+                                    if (line < self.size().height) {
+                                        self.cursor.pos = .{ .x = @intCast(u32, pos) - buf_pos, .y = line };
+                                        self.offset = .{ .x = 0, .y = 0 };
+                                        return;
+                                    }
+
+                                    self.cursor.pos = .{ .x = @intCast(u32, pos) - buf_pos, .y = self.size().height / 2 };
+                                    self.offset = .{ .x = 0, .y = line - self.size().height / 2 };
+                                }
+                                buf_pos += @intCast(u32, l.len) + 1;
+                                line += 1;
+                            }
+                        }
                     },
                 },
                 .insertion => |c| {
@@ -601,187 +621,189 @@ pub const InputHandler = struct {
             if (inhibit_clear_repeat) break :b;
             self.repeat.clearRetainingCapacity();
         }
-        instructions[0] = Instruction{
-            .movement = .{
-                .movement = switch (self.mode) {
-                    .normal => |state| switch (state) {
-                        .none => switch (c) {
-                            'j' => Movement.down,
-                            'k' => Movement.up,
-                            'l' => Movement.right,
-                            'h' => Movement.left,
-                            'w' => Movement.word_right,
-                            'b' => Movement.word_left,
-                            2 => Movement.page_up,
-                            6 => Movement.page_down,
-                            'g' => b: {
-                                if (self.repeat.items.len != 0) break :b Movement.goto_line;
+        const movement = switch (self.mode) {
+            .normal => |state| switch (state) {
+                .none => switch (c) {
+                    'j' => Movement.down,
+                    'k' => Movement.up,
+                    'l' => Movement.right,
+                    'h' => Movement.left,
+                    'w' => Movement.word_right,
+                    'b' => Movement.word_left,
+                    2 => Movement.page_up,
+                    6 => Movement.page_down,
+                    'g' => b: {
+                        if (self.repeat.items.len != 0) break :b Movement.goto_line;
 
-                                self.mode = .{ .normal = .goto };
-                                return instructions;
+                        self.mode = .{ .normal = .goto };
+                        return instructions;
+                    },
+                    'v' => {
+                        self.mode = .{ .normal = .viewport };
+                        return instructions;
+                    },
+                    'V' => {
+                        self.mode = .{ .normal = .viewport_sticky };
+                        return instructions;
+                    },
+                    ':' => {
+                        self.mode = .command;
+                        return instructions;
+                    },
+                    '/' => {
+                        self.mode = .search;
+                        return instructions;
+                    },
+                    'i' => {
+                        self.mode = .insert;
+                        return instructions;
+                    },
+                    'a', 'A' => {
+                        self.mode = .insert;
+                        instructions[0] = .{ .movement = .{
+                            .movement = switch (c) {
+                                'a' => Movement.right,
+                                'A' => Movement.goto_line_end_plus_one,
+                                else => unreachable,
                             },
-                            'v' => {
-                                self.mode = .{ .normal = .viewport };
-                                return instructions;
-                            },
-                            'V' => {
-                                self.mode = .{ .normal = .viewport_sticky };
-                                return instructions;
-                            },
-                            ':' => {
-                                self.mode = .command;
-                                return instructions;
-                            },
-                            '/' => {
-                                self.mode = .search;
-                                return instructions;
-                            },
-                            'i' => {
-                                self.mode = .insert;
-                                return instructions;
-                            },
-                            'a', 'A' => {
-                                self.mode = .insert;
-                                instructions[0] = .{ .movement = .{
-                                    .movement = switch (c) {
-                                        'a' => Movement.right,
-                                        'A' => Movement.goto_line_end_plus_one,
-                                        else => unreachable,
-                                    },
-                                    .opts = .{ .allow_past_last_column = true },
-                                } };
-                                return instructions;
-                            },
-                            'o' => {
-                                self.mode = .insert;
+                            .opts = .{ .allow_past_last_column = true },
+                        } };
+                        return instructions;
+                    },
+                    'o' => {
+                        self.mode = .insert;
 
-                                instructions[0] = Instruction{
-                                    .movement = .{
-                                        .movement = Movement.goto_line_end_plus_one,
-                                        .opts = .{ .allow_past_last_column = true },
-                                    },
-                                };
-                                instructions[1] = Instruction{ .insertion = 13 };
-                                return instructions;
+                        instructions[0] = Instruction{
+                            .movement = .{
+                                .movement = Movement.goto_line_end_plus_one,
+                                .opts = .{ .allow_past_last_column = true },
                             },
-                            'O' => {
-                                self.mode = .insert;
-                                instructions[0] = .{
-                                    .movement = .{
-                                        .movement = Movement.goto_line_start,
-                                        .opts = .{ .allow_past_last_column = true },
-                                    },
-                                };
-                                instructions[1] = .{ .insertion = 13 };
-                                instructions[2] = .{ .movement = .{ .movement = Movement.up } };
-                                instructions[3] = .{ .movement = .{ .movement = Movement.goto_line_start } };
-                                return instructions;
-                            },
-                            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' => {
-                                inhibit_clear_repeat = true;
-                                try self.repeat.append(self.gpa, c);
-                                return instructions;
-                            },
-                            else => return instructions,
-                        },
-                        .goto => switch (c) {
-                            'e' => blk: {
-                                self.mode = .{ .normal = .none };
-                                break :blk Movement.goto_file_end;
-                            },
-                            'g' => blk: {
-                                self.mode = .{ .normal = .none };
-                                break :blk Movement.goto_file_top;
-                            },
-                            'h' => blk: {
-                                self.mode = .{ .normal = .none };
-                                break :blk Movement.goto_line_start;
-                            },
-                            'l' => blk: {
-                                self.mode = .{ .normal = .none };
-                                break :blk Movement.goto_line_end;
-                            },
-                            else => {
-                                self.mode = .{ .normal = .none };
-                                return instructions;
-                            },
-                        },
-                        .viewport, .viewport_sticky => b: {
-                            const movement = switch (c) {
-                                'j' => Movement.viewport_down,
-                                'k' => Movement.viewport_up,
-                                't' => Movement.viewport_line_top,
-                                'c' => Movement.viewport_centre,
-                                'b' => Movement.viewport_line_bottom,
-                                else => {
-                                    self.mode = .{ .normal = .none };
-                                    return instructions;
-                                },
-                            };
-                            if (state == .viewport) {
-                                self.mode = .{ .normal = .none };
-                            }
-                            break :b movement;
-                        },
+                        };
+                        instructions[1] = Instruction{ .insertion = 13 };
+                        return instructions;
                     },
-                    .command => switch (c) {
-                        13 => {
-                            // RET
-                            const al = self.cmd.toManaged(self.gpa);
-                            self.cmd = .{};
-                            self.mode = .{ .normal = .none };
-                            instructions[0] = .{ .command = al };
-                            return instructions;
-                        },
-                        27 => {
-                            // ESC
-                            self.mode = .{ .normal = .none };
-                            self.cmd.clearRetainingCapacity();
-                            return instructions;
-                        },
-                        else => {
-                            if (c < 32 or c > 126) return instructions;
-                            try self.cmd.append(self.gpa, c);
-                            return instructions;
-                        },
+                    'O' => {
+                        self.mode = .insert;
+                        instructions[0] = .{
+                            .movement = .{
+                                .movement = Movement.goto_line_start,
+                                .opts = .{ .allow_past_last_column = true },
+                            },
+                        };
+                        instructions[1] = .{ .insertion = 13 };
+                        instructions[2] = .{ .movement = .{ .movement = Movement.up } };
+                        instructions[3] = .{ .movement = .{ .movement = Movement.goto_line_start } };
+                        return instructions;
                     },
-                    .search => switch (c) {
-                        13 => {
-                            // RET
-                            const al = self.cmd.toManaged(self.gpa);
-                            self.cmd = .{};
-                            self.mode = .{ .normal = .none };
-                            instructions[0] = .{ .search = .{ .complete = al } };
-                            return instructions;
-                        },
-                        27 => {
-                            // ESC
-                            self.mode = .{ .normal = .none };
-                            self.cmd.clearRetainingCapacity();
-                            instructions[0] = .{ .search = .quit };
-                            return instructions;
-                        },
-                        else => {
-                            if (c < 32 or c > 126) return instructions;
-                            try self.cmd.append(self.gpa, c);
-                            instructions[0] = .{ .search = .char };
-                            return instructions;
-                        },
+                    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' => {
+                        inhibit_clear_repeat = true;
+                        try self.repeat.append(self.gpa, c);
+                        return instructions;
                     },
-                    .insert => switch (c) {
-                        27 => {
-                            // ESC
-                            self.mode = .{ .normal = .none };
-                            // Not sure why we need to do this - possible miscomp?
-                            instructions[0] = .noop;
-                            return instructions;
-                        },
-                        else => {
-                            instructions[0] = .{ .insertion = c };
-                            return instructions;
-                        },
+                    else => return instructions,
+                },
+                .goto => switch (c) {
+                    'e' => blk: {
+                        self.mode = .{ .normal = .none };
+                        break :blk Movement.goto_file_end;
+                    },
+                    'g' => blk: {
+                        self.mode = .{ .normal = .none };
+                        break :blk Movement.goto_file_top;
+                    },
+                    'h' => blk: {
+                        self.mode = .{ .normal = .none };
+                        break :blk Movement.goto_line_start;
+                    },
+                    'l' => blk: {
+                        self.mode = .{ .normal = .none };
+                        break :blk Movement.goto_line_end;
+                    },
+                    else => {
+                        self.mode = .{ .normal = .none };
+                        return instructions;
                     },
                 },
+                .viewport, .viewport_sticky => b: {
+                    const movement = switch (c) {
+                        'j' => Movement.viewport_down,
+                        'k' => Movement.viewport_up,
+                        't' => Movement.viewport_line_top,
+                        'c' => Movement.viewport_centre,
+                        'b' => Movement.viewport_line_bottom,
+                        else => {
+                            self.mode = .{ .normal = .none };
+                            return instructions;
+                        },
+                    };
+                    if (state == .viewport) {
+                        self.mode = .{ .normal = .none };
+                    }
+                    break :b movement;
+                },
+            },
+            .command => switch (c) {
+                13 => {
+                    // RET
+                    const al = self.cmd.toManaged(self.gpa);
+                    self.cmd = .{};
+                    self.mode = .{ .normal = .none };
+                    instructions[0] = .{ .command = al };
+                    return instructions;
+                },
+                27 => {
+                    // ESC
+                    self.mode = .{ .normal = .none };
+                    self.cmd.clearRetainingCapacity();
+                    return instructions;
+                },
+                else => {
+                    if (c < 32 or c > 126) return instructions;
+                    try self.cmd.append(self.gpa, c);
+                    return instructions;
+                },
+            },
+            .search => switch (c) {
+                13 => {
+                    // RET
+                    const al = self.cmd.toManaged(self.gpa);
+                    self.cmd = .{};
+                    self.mode = .{ .normal = .none };
+                    instructions[0] = .{ .search = .{ .complete = al } };
+                    return instructions;
+                },
+                27 => {
+                    // ESC
+                    self.mode = .{ .normal = .none };
+                    self.cmd.clearRetainingCapacity();
+                    instructions[0] = .{ .search = .quit };
+                    return instructions;
+                },
+                else => {
+                    if (c < 32 or c > 126) return instructions;
+                    try self.cmd.append(self.gpa, c);
+                    instructions[0] = .{ .search = .char };
+                    return instructions;
+                },
+            },
+            .insert => switch (c) {
+                27 => {
+                    // ESC
+                    self.mode = .{ .normal = .none };
+                    // Not sure why we need to do this - possible miscomp?
+                    instructions[0] = .noop;
+                    return instructions;
+                },
+                else => {
+                    instructions[0] = .{ .insertion = c };
+                    return instructions;
+                },
+            },
+        };
+
+        instructions[0] = Instruction{
+            .movement = .{
+                .movement = movement,
                 .opts = .{
                     .repeat = if (self.repeat.items.len == 0) 1 else try std.fmt.parseInt(u32, self.repeat.items, 10),
                 },
